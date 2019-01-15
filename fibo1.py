0d370c297aa015ddc50646bc85c57b700acee748
skysuraj <46665462+skysuraj@users.noreply.github.com>
2019-01-15 16:20:08 +0545

    Add files via upload

---
+++ fibo1.py
@@ -0,0 +1,10 @@
+cache = {}
+def fiba(n):
+     cache[n] = cache.get(n, 0) or (n <= 1 and 1 or fiba(n-1) + fiba(n-2))
+     return cache[n]
+n = 0
+x = 0
+while fiba(x) <= 4000000:
+       if not fiba(x) % 2: n = n + fiba(x)
+       x=x+1
+print(n)
